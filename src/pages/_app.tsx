54af699729672e241c79e2eb07dfba3e55d5d366
Ayush Jain <ayushjain2205@gmail.com>
2024-11-12 07:25:30 +0700

    Orbitron config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Orbitron } from "next/font/google";
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={orbitron.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
